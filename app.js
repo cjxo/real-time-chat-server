2b8567b07cf743c85d11938a7470b085a76c4f32
Christian <christianjao88@gmail.com>
2025-01-25 13:28:02 +0800

    getting ready for prod

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const messageRouter = require("./routes/message");
 const { app, server } = require("./lib/socket.js");
+const path = require("node:path");
 
 app.use(cors({
   origin: ["http://localhost:5173"],
@@ -31,6 +32,14 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: err.stack });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
  server.listen(3000, () => {
   console.log("Server Listening at port 3000");
